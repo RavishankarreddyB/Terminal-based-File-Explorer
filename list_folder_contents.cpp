933d8c3a388c724738f863a21956bad28e633fa1
Ravi Shankar Reddy Bommana <ravishankarreddy.bommana@citrix.com>
2021-01-04 22:44:56 +0530

    along with filenames - size and file type are also shown

--- list_folder_contents.cpp
+++ list_folder_contents.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <iostream>
 #include <filesystem>
-
+#include <cmath>
 #include <algorithm>
 
 using namespace std;
@@ -14,12 +14,35 @@ int main()
     for (const auto & entry : filesystem::directory_iterator(current_path)) {
 	// to display full file path
 	//cout << entry.path() << endl;
+	
+	filesystem::directory_entry de(entry.path());
+	if( de.is_directory() )
+		cout<<"dir\t";
+	else if( de.is_regular_file() )
+		cout<<"file\t";
 
 	// to display just the name
 	string fileName = entry.path().filename();
 
 	// to remove quotes while printing using cout
 	fileName.erase(remove(fileName.begin(), fileName.end(), '\"'), fileName.end());
-	cout << fileName << endl;
+	cout << fileName << "\t";
+	
+	string size_chart = "BKMGTP"; // K - Kilobyte, M - Megabyte, G - Gigabyte, T - Terabyte, P - Petabyte;
+	
+	if( !de.is_directory() ) {
+		cout << de.file_size() << "\t";
+		// convert to human readable
+		int i=0;
+		double size = de.file_size();
+		while(size >= 1024) {
+			size=size/1024;
+			i++;
+		}
+		cout << fixed << setprecision( 2 ) << size;
+		if ( i > 0 )
+			cout << size_chart[i];
+		cout << "B" << endl;
+	}
     }
 }
